fe79795856276e410035a6f66eabda4e54b9a5da
Lauren Mendoza <laurenmendoza@gmail.com>
2023-03-08 20:02:51 -0800

    bootstrap classes

--- client/src/Components/list-item.js
+++ client/src/Components/list-item.js
@@ -3,18 +3,21 @@ import React from "react";
 export default function ListItem(props){
 	return (
 		<div className="list--item">
-			<input 
-				type="checkbox"
-				checked={props.checked}
-				onChange={(event)=>{props.handleChange(event,props.id)}}
-			/>
-			<input 
-				type="text"
-				placeholder={props.value}
-				className="form--input"
-				value={props.value}
-				onChange={(event)=>{props.handleChange(event,props.id)}}
-			/>	
+			<div className="form-check">
+				<input 
+					className="form-check-input"
+					type="checkbox"
+					checked={props.checked}
+					onChange={(event)=>{props.handleChange(event,props.id)}}
+				/>
+				<input 
+					type="text"
+					placeholder={props.value}
+					className="form-control"
+					value={props.value}
+					onChange={(event)=>{props.handleChange(event,props.id)}}
+				/>
+			</div>
 		</div>
 	)
 }
